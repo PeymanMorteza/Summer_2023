9482474bd85517d24d708071210e1809b6a6c395
Peyman Morteza <PeymanMorteza@users.noreply.github.com>
2023-06-02 03:46:36 -0500

    Rotation

---
+++ SortedCollectionInterface.java
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>302 Found</title>
+</head><body>
+<h1>Found</h1>
+<p>The document has moved <a href="https://pages.cs.wisc.edu/~cs400/SortedCollectionInterface.java">here</a>.</p>
+</body></html>
